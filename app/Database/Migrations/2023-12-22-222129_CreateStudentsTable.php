50fabb0a614e862b450bcc8e574379dcd0f7ba90
Fernando de Oliveira Neto <fernando.oliveira2911@gmail.com>
2023-12-23 10:42:07 -0300

    feat: students table creation migration

---
+++ app/Database/Migrations/2023-12-22-222129_CreateStudentsTable.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateStudentsTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true
+            ],
+            'name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'email' => [
+                'type' => 'VARCHAR',
+                'constraint' => '100',
+                'unique' => true,
+            ],
+            'phone' => [
+                'type' => 'VARCHAR',
+                'constraint' => '50',
+            ],
+            'address' => [
+                'type' => 'TEXT',
+            ],
+            'photo' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+                'null' => true,
+            ],
+            'created_at datetime default current_timestamp',
+            'updated_at datetime default current_timestamp on update current_timestamp',
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('students');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('students');
+    }
+}
