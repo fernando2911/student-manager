701b8be8ef2372f6f8cdc32ba10a8bded34059cc
Fernando de Oliveira Neto <fernando.oliveira2911@gmail.com>
2023-12-23 11:00:50 -0300

    feat: student creation endpoint

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,3 +7,4 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 $routes->get('students', 'StudentController::index', ['filter' => 'dbCheck']);
+$routes->post('student/create', 'StudentController::create', ['filter' => 'dbCheck']);
--- app/Controllers/StudentController.php
+++ app/Controllers/StudentController.php
@@ -17,4 +17,31 @@ class StudentController extends ResourceController
 
         return $this->respond($students);
     }
+
+    public function create()
+    {
+        $json = $this->request->getJSON();
+
+        $model = new StudentModel();
+
+        $data = [
+            'name'    => $json->name,
+            'email'   => $json->email,
+            'phone'   => $json->phone,
+            'address' => $json->address,
+            // Implementar upload de fotos
+        ];
+
+        $model->insert($data);
+
+        $response = [
+            'status'   => 201,
+            'error'    => null,
+            'messages' => [
+                'success' => 'Estudante criado com sucesso'
+            ]
+        ];
+        
+        return $this->respond($response);
+    }
 }
